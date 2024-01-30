62d435c6f202b16caee4f9e0290ff86b95016c84
k-cardon <cardon.kristen.n@gmail.com>
2024-01-30 15:50:19 -0800

    added link field to project model and template

--- projects/models.py
+++ projects/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
+    link = models.URLField(max_length=200, blank=True)
     image = models.FileField(upload_to="project_images/", blank=True) 
     
     def __str__(self):
