0cc54c23bae3eeea0e53bc692100b1322d65cc3b
k-cardon <cardon.kristen.n@gmail.com>
2024-03-17 22:12:12 -0700

    adds sendgrid email

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -174,3 +174,15 @@ MEDIA_URL = "media/"
 SESSION_COOKIE_SECURE = True
 
 CSRF_COOKIE_SECURE = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+
+EMAIL_HOST_USER = 'apikey'
+
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
